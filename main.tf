4958c18924c444b133c56d1eaf7aca9826eccfd7
satyakanth143 <45382178+satyakanth143@users.noreply.github.com>
2025-03-26 19:10:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
-key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+key_name = "satya"
+vpc_security_group_ids = ["sg-0e972c119147390e0"]
 tags = {
 Name = var.instance_names[count.index]
 }
