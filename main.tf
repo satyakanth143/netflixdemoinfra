f7826aaedb6038fd095a4a73a1529a53b514a048
satyakanth143 <45382178+satyakanth143@users.noreply.github.com>
2025-03-26 19:26:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
 key_name = "satya"
-vpc_security_group_ids = ["sg-0e972c119147390e0"]
+vpc_security_group_ids = ["sg-0946f285fe903bc89"]
 tags = {
 Name = var.instance_names[count.index]
 }
